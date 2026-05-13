e6ae84a290397491ff3ba77aa73f6c8e473c56bf
Muhammad Rizqi Pratama <awsgar55@gmail.com>
2026-05-13 20:53:22 +0700

    memasukan semua code kedalam file tentang.php

---
+++ tentang.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tentang</title>
+      <link rel="stylesheet" href="assets/style.css">
+
+</head>
+</head>
+<body>
+    <div class="container">
+        <img src="assets/img/Logo-warung3.png" class="logo" alt="Logo warung" width="100px" height="80px">
+    </div>
+
+    <header>
+        <nav>
+           <button><a href="tentang.html">Tentang</a></button>
+           <button><a href="transaction/layanan.php">Layanan</a></button> 
+           <button><a href="transaction/hubungi.php">Hubungi</a></button>
+           <button><a href="order.html">Order</a></button>
+            
+        </nav>
+    </header>
+
+    <div class="tesk">
+        <p>Ini adalah halaman utama kami tentang Warung Nusantara yang menghasilkan berbagai banyak makanan khas indonesia, sama khsasnya, kalian juga
+            bisa memesan Makanan yang ada dari warung nusantara ini Secara Online, atau juga kalian bisa datang Langsung ke tempat kami, Serta Kami menydiakan
+            Layanan Antar jemput untuk pesanan kalian yang Berada di sekitaran Wilayah kami.dan Jika mengalami keluhan terhadap layanan kami atau menu makanan yang
+            kami sediakan, kalian bisa hubungin kami di halaman Hubungin kami.
+        </p>
+    </div>
+    
+    <div class="container1">
+        <img src="assets/img/Nasi padang.jpg" class="gerak"><br>
+        <br>
+        <img src="assets/img/Soto Ayam.jpg" class="gerak"><br>
+        <br>
+        <img src="assets/img/nasi goreng.jpg" class="gerak">
+    </div>
+
+    <div class="hero">
+        <img src="assets/img/teh panas.jpg" class="gerak"><br> 
+        <br>
+        <img src="assets/img/kopi hitam.jpg" class="gerak">
+        </div>
+ 
+    
+    <footer>
+        <p>&copy; 2025 warung makanan kami buka sampai jam 22.00 JL.KP Muara KEC Kutawaringin RT 1 RW 11</p>
+    </footer>
+</body>
+</html>
+
+
+<?php
+
+?>

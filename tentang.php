4e6f748593df4cd0833a21782e8dafd24d6d4a9f
Muhammad Rizqi Pratama <awsgar55@gmail.com>
2026-05-16 14:58:31 +0700

    memperbaiki bagian yang salah

--- tentang.php
+++ tentang.php
@@ -8,7 +8,7 @@ include 'auth/config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tentang</title>
-      <link rel="stylesheet" href="assets/style.css">
+      <link rel="stylesheet" href="../assets/style.css">
 
 </head>
 </head>

f2b71e20a04028f9550968514ee3a776386f0f13
Muhammad Rizqi Pratama <awsgar55@gmail.com>
2026-05-19 10:05:27 +0700

    koneksikan kedalam database dan juga menambahkan html

---
+++ Transaction/hubungi.php
@@ -0,0 +1,28 @@
+<?php 
+include 'auth/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hubungi</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <img src="../assets/img/Logo-warung3.png" class="logo" alt="Logo warung" width="100px" height="80px">
+    </div>
+    
+     <header>
+        <nav>
+           <button><a href="../tentang.html">Tentang</a></button>
+           <button><a href="Hubungi.html">Hubungi</a></button>
+           <button><a href="../order.html">order</a></button>
+            
+        </nav>
+    </header>
+    
+</body>
+</html>

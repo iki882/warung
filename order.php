4f4a5978edb9e0ed709d5ac70370ec9ef247ae29
Muhammad Rizqi Pratama <awsgar55@gmail.com>
2026-06-01 19:34:37 +0700

    menambahkan width dan juga height kedalam img src

--- order.php
+++ order.php
@@ -46,7 +46,7 @@
     <div class="item">
         <h1>2.Mie goreng</h1>
         <p>10000</p>
-        <img src="assets/img/SKINNYASIA_ Mi Goreng.jpg" class="img" alt="mie goreng" width="100px" heigth="80px">
+        <img src="assets/img/SKINNYASIA_ Mi Goreng.jpg" class="img" alt="mie goreng" width="100px" height="80px">
         <a href="admin/kasir.php?menu=Mie goreng&harga=10000">
         <button class="btn">Beli Sekarang</button>
         </a>
@@ -54,7 +54,7 @@
     <div class="item">
         <h1>3.Sate Ayam</h1>
         <p>25000</p>
-        <img src="assets/img/sate ayam.jpg" class="img" alt="sate ayam" width="100px" heigth="80px">
+        <img src="assets/img/sate ayam.jpg" class="img" alt="sate ayam" width="100px" height="80px">
         <a href="admin/kasir.php?menu=Sate ayam&harga=25000">
         <button class="btn">beli Sekarang</button>
         </a>
@@ -62,7 +62,7 @@
     <div class="item">
         <h1>4.Soto Ayam</h1>
         <p>15000</p>
-        <img src="assets/img/Soto Ayam.jpg" class="img" alt="Soto Ayam" width="100px" heigth="80px">
+        <img src="assets/img/Soto Ayam.jpg" class="img" alt="Soto Ayam" width="100px" height="80px">
         <a href="admin/kasir.php?menu=soto ayam&harga=15000">
         <button class="btn">beli Sekarang</button>
         </a>
@@ -70,7 +70,7 @@
     <div class="item">
         <h1>5.Nasi Padang</h1>
         <p>23000</p>
-        <img src="assets/img/Nasi padang.jpg" class="img" alt="Nasi padang" width="100px" heigth="80px">
+        <img src="assets/img/Nasi padang.jpg" class="img" alt="Nasi padang" width="100px" height="80px">
           <a href="admin/kasir.php?menu=Nasi Padang&harga=23000">
           <button class="btn">Beli Sekarang</button>
           </a>
@@ -83,7 +83,7 @@
     <div class="item">
         <h1>1. Es Teh</h1>
         <p>5000</p>  
-        <img src="assets/img/es teh.jpg" class="img" alt="es teh" width="100px" heigth="80px">
+        <img src="assets/img/es teh.jpg" class="img" alt="es teh" width="100px" height="80px">
         <a href="admin/kasir.php?menu=Es Teh&harga=5000">
         <button class="btn">Beli Sekarang</button>
     </a>    
@@ -91,7 +91,7 @@
     <div class="item">
         <h1>2. Teh Panas</h1>
         <p>50000</p>
-        <img src="assets/img/teh panas.jpg" class="img" alt="teh panas" width="100px" heigth="80px">
+        <img src="assets/img/teh panas.jpg" class="img" alt="teh panas" width="100px" height="80px">
         <a href="admin/kasir.php?menu=Teh Panas&harga=50000">
         <button class="btn">Beli Sekarang</button>
         </a>
@@ -100,7 +100,7 @@
     <div class="item">
         <h1>3. Kopi hitam</h1>
         <p>8000</p>
-        <img src="assets/img/kopi hitam.jpg" class="img" alt="kopi hitam" width="100px" heigth="80px">
+        <img src="assets/img/kopi hitam.jpg" class="img" alt="kopi hitam" width="100px" height="80px">
         <a href="admin/kasir.php?menu=Kopi hitam&harga=8000">
         <button class="btn">Beli Sekarang</button>
         </a>
@@ -108,7 +108,7 @@
     <div class="item">
         <h1>4.Air Mineral</h1>
         <p>7000</p>
-        <img src="assets/img/air mineral.jpg" class="img" alt="air mineral" width="100px" heigth="80px">
+        <img src="assets/img/air mineral.jpg" class="img" alt="air mineral" width="100px" height="80px">
         <a href="admin/kasir.php?menu=Air Mineral&harga=7000">
         <button class="btn">Beli Sekarang</button>
         </a>
@@ -116,7 +116,7 @@
     <div class="item">
         <h1>5. Jus Alpukat</h1>
         <p>15000</p>
-        <img src="assets/img/jus alpukat.jpg" class="img" alt="jus alpukat" width="100px" heigth="80px">
+        <img src="assets/img/jus alpukat.jpg" class="img" alt="jus alpukat" width="100px" height="80px">
         <a href="admin/kasir.php?menu=Jus Alpukat&harga=15000">
          <button class="btn">beli Sekarang</button>
         </a>
@@ -129,10 +129,10 @@
     
     <div class="sosmed">
         <a href="#" class="icon">
-            <img src="assets/img/WhastAPP.jpg" alt="whatsApp">
+            <img src="assets/img/WhastAPP.jpg" alt="whatsApp" width="100px" height="80px">
         </a>
         <a href="#" class="icon">
-            <img src="assets/img/logo instagram.png" alt="instagram">
+            <img src="assets/img/logo instagram.png" alt="instagram" width="100px" height="80px">
         </a>
     </div>
     
